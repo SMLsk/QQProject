c046029d4a379f4ca39b44bf5f68d04cd9e44c59
SMLsk <2532512586@qq.com>
2020-07-21 22:08:49 +0800

    7_21_22:08

--- other/test.go
+++ other/test.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"fmt"
-	// "github.com/sciter-sdk/go-sciter"
-	// "github.com/sciter-sdk/go-sciter/window"
-)
-
-type FriendsManager struct {
-	Groups        map[int]string
-	GroupsFriends map[int]int
-}
-
-var (
-	MyFriendsManager *FriendsManager
+	// "fmt"
+	"github.com/sciter-sdk/go-sciter"
+	"github.com/sciter-sdk/go-sciter/window"
 )
 
 func main() {
-	fmt.Println(MyFriendsManager)
+	Window, err := window.New(sciter.DefaultWindowCreateFlag, &sciter.Rect{1190, 70, 1480, 630})
+	if err != nil {
+		// log.Fatal(err)
+	}
+	Window.LoadFile("E://Technology//Project//MyGo//src//QQ//application//client//view//mainTest.html")
+	Window.SetTitle("Main")
+	Window.Show()
+	Window.Run()
 }
